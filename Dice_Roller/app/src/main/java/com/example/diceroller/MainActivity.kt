29fbcc192bf86c5200e74a6f56fe1b9dbac2aade
Sumit <sumitdahiya1901@gmail.com>
2021-05-13 23:13:53 +0530

    Tip calculator

---
+++ Dice_Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.diceroller.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculatetip() }
+    }
+
+
+    private fun calculatetip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        if (cost == null){
+            binding.tipResult.text=""
+            return}
+        val tipPrecentage = when (binding.tipOption.checkedRadioButtonId) {
+            R.id.option_15_percent -> 0.15
+            R.id.option_18_percent -> 0.18
+            else -> 0.20
+        }
+        var tip = tipPrecentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+
+}
+
