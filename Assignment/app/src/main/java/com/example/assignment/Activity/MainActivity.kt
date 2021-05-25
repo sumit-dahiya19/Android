3ec6d08c4cfaacb156be31443661846f926e77cc
Sumit <sumitdahiya1901@gmail.com>
2021-05-25 14:11:39 +0530

    maps with api

---
+++ Assignment/app/src/main/java/com/example/assignment/Activity/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.assignment
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.assignment.Network.ApiService
+import com.example.assignment.Network.ServiceBuilder
+import com.example.assignment.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val service = ServiceBuilder.buildService(ApiService::class.java)
+        val requestcall = service.getdata()
+        requestcall.enqueue(object : Callback<VechicleInfo> {
+            override fun onFailure(call: Call<VechicleInfo>, t: Throwable) {
+                Toast.makeText(baseContext, "Failed to fetch data", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(call: Call<VechicleInfo>, response: Response<VechicleInfo>) {
+                if (response.isSuccessful) {
+                    val adapter = MyAdapter(response.body()!!)
+                    binding.recyclerView.adapter = adapter
+                    binding.recyclerView.layoutManager=LinearLayoutManager(baseContext)
+
+                } else
+                    Toast.makeText(baseContext, "Some Error Occured ", Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+    }
+}
+
+/* TO PRINT API DATA IN SINGLE TEXTVIEW IN ANDROID APP
+
+fun loadDataApi(binding: ActivityMainBinding) {
+    val service = ServiceBuilder.buildService(ApiService::class.java)
+    val requestcall = service.getdata()
+    requestcall.enqueue(object : Callback<String> {
+
+        override fun onFailure(call: Call<String>, t: Throwable) {
+            //Toast.makeText(parent.context,"Failed",Toast.LENGTH_LONG).show()
+        }
+
+        override fun onResponse(call: Call<String>, response: Response<String>) {
+            if(response.isSuccessful){
+                 var result= response.body().toString()!!
+                binding.apidata.text=result
+            }
+        }
+    })
+}
+*/
+
---
+++ Assignment/app/src/main/java/com/example/assignment/Network/ServiceBuilder.kt
@@ -0,0 +1,30 @@
+package com.example.assignment.Network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private const val URL = "https://run.mocky.io/"
+
+    private val okHttp = OkHttpClient.Builder()
+
+    private val moshi = Moshi
+        .Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val builder = Retrofit.Builder().baseUrl(URL)
+        .addConverterFactory(
+            GsonConverterFactory.create()
+        )
+        .client(okHttp.build())
+
+    private val retrofit = builder.build()
+
+    fun <T> buildService(servicetype: Class<T>): T {
+        return retrofit.create(servicetype)
+    }
+}
