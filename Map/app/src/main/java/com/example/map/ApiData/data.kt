366425a1427d6b94b4971874a71414008f598889
Sumit <sumitdahiya1901@gmail.com>
2021-05-25 15:10:25 +0530

    InfoWindow on top of map marker showing the speed you are getting from the API

---
+++ Map/app/src/main/java/com/example/map/ApiData/data.kt
@@ -0,0 +1,63 @@
+package com.example.map
+
+import com.google.gson.annotations.SerializedName
+
+data class VechicleInfo(
+    val success: Boolean,
+    val data: List<Data>,
+    val meta: Meta
+)
+
+data class Data(
+    val id: Long,
+
+    @SerializedName("display_number")
+    val displayNumber: String,
+
+    @SerializedName("icon_kind")
+    val iconKind: String,
+
+    @SerializedName("last_location")
+    val lastLocation: LastLocation,
+
+    @SerializedName("icon_details")
+    val iconDetails: IconDetails,
+
+    @SerializedName("vehicle_make")
+    val vehicleMake: Any? = null,
+
+    @SerializedName("vehicle_model")
+    val vehicleModel: Any? = null
+
+
+)
+
+data class IconDetails(
+
+    @SerializedName( "icon_kind")
+    val iconKind: String,
+
+    @SerializedName( "vehicle_icon_id")
+    val vehicleIconID: Any? = null,
+
+    @SerializedName( "is_custom")
+    val isCustom: Boolean,
+
+    @SerializedName("service_url")
+    val serviceURL: String
+)
+
+data class LastLocation(
+    val lat: Double? ,
+    val long: Double?,
+    val orientation: Double?,
+    val speed: Double?,
+
+    @SerializedName( "received_ts")
+    val receivedTs: Long? = null
+)
+
+data class Meta(
+    val total: Long
+)
+
---
+++ Map/app/src/main/java/com/example/map/Network/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.map
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("https://run.mocky.io/v3/f55d3019-2997-4153-b029-ed5aeaee8bfa")
+    fun getdata(): Call<VechicleInfo>
+}
